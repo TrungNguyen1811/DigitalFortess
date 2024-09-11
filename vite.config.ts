87af2697c6bd6377f5d908cdaaa015730ef3e5e0
TrungNguyen1811 <trung4atlam@gmail.com>
2024-09-11 12:35:20 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path';
  * @see https://vitejs.dev/config/
  */
 export default defineConfig({
-  base: '/',
+  // base: '/',
   plugins: [react(), eslintPlugin()],
   resolve: {
     alias: {
